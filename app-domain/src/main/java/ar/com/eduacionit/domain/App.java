bd3a9b4e48775119ca7714c1ffa7fb10415ee5bf
agustin <agustin@192.168.0.14>
2020-09-15 22:00:48 -0300

    clase 1
    Creacion de entorno de trabajo

---
+++ app-domain/src/main/java/ar/com/eduacionit/domain/App.java
@@ -0,0 +1,13 @@
+package ar.com.eduacionit.domain;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
